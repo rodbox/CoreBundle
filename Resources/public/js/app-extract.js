98c1b843fd55ef799f1427953814cce751019b95
Rodolphe <jacquemin.rodolphe@hotmail.fr>
2016-07-12 21:28:17 +0200

    fix

--- Resources/public/js/app-extract.js
+++ Resources/public/js/app-extract.js
@@ -1,8 +1,7 @@
 $(document).on("keypress",".extract-content",function (e){
-	e.preventDefault();	
 	var t = $(this);
-
 	if (e.keyCode == 13) {
+		e.preventDefault();	
 		var list = t.val().split('\n');
 		$.cb[t.data('cb-app')][t.data('cb')](t, e, list);
 	}
