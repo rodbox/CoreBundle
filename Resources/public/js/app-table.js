259efc3cfbbe95e8e383532f6875e3508ba04379
rodbox <rodolphe@rodbox.fr>
2016-07-29 13:05:32 +0200

    fix table sys

--- Resources/public/js/app-table.js
+++ Resources/public/js/app-table.js
@@ -9,17 +9,17 @@ $(document).on("dblclick keypress focusout",".table-ceil-edit",function (e){
 	// on quitte : on bloque le champ
 	else if(e.type=="focusout"){
 		t.val(t.attr('data-value'));
-		t.attr('readonly');
+		t.attr('readonly',true);
 	}
 	else {
 		if (e.keyCode == 13){
 			t.attr('data-value',t.val());
-			t.attr('readonly');
+			t.attr('readonly',true);
 		}
 
 		if (e.keyCode == 27) {
 			t.val(t.attr('data-value'));
-			t.attr('readonly');
+			t.attr('readonly',true);
 		}
 	}
 })
