022ae74807891c3b9ee8e39036cc6a148cd9d026
Rodolphe <jacquemin.rodolphe@hotmail.fr>
2016-08-27 17:57:40 +0200

    pagination system select

--- Resources/public/js/app-pagination.js
+++ Resources/public/js/app-pagination.js
@@ -92,4 +92,19 @@ $(document).on("click",".pagination a.page-link, th.sorted a, th a.sortable",fun
       history.pushState(null, t.attr('title'), url);
       var url = location.href;
   });
-})
+});
+
+$(document).on("change",".pagination select.page-link",function (e){
+  e.preventDefault();
+  var t = $(this);
+  var url = t.data('url');
+  var data = {
+    page:t.val()
+  };
+
+  $.get(url,data, function(html) {
+      $("#app-content").html(html);
+      history.pushState(null, t.attr('title'), url);
+      var url = location.href;
+  });
+});
